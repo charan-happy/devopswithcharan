122d3c84f630728d2b83c48d8d0bb0f6b0348d20
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2024-03-26 06:31:13 +0530

    Update scripts
    Added script on how to create a file

---
+++ shell script/scripts/FileCreation.sh
@@ -0,0 +1,21 @@
+echo "Enter your file name"
+
+read employeename
+
+for((i=0;i<=5;i++)){
+touch $dirname$i
+}
+
+----------------------------
+output :
+
+Enter your file name
+
+charan
+
+charan1
+charan2
+charan3
+charan4
+charan5
+
